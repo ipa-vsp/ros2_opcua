9afcccc4370cc2fc30b4c2dca7e75fc0f42dc584
ipa-vsp <vishnu.pbhat93@gmail.com>
2024-04-25 08:49:31 +0200

    pre-commit update

--- opcua_tests/src/example_client_node.cpp
+++ opcua_tests/src/example_client_node.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-#include "rclcpp/rclcpp.hpp"
-#include "open62541pp/open62541pp.h"
 #include "opcua_tests/custom_datatype.h"
+#include "open62541pp/open62541pp.h"
+#include "rclcpp/rclcpp.hpp"
 
-
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     rclcpp::init(argc, argv);
 
     rclcpp::NodeOptions options;
@@ -16,30 +16,31 @@ int main(int argc, char* argv[]) {
     exec->add_node(node_);
 
     opcua::Client client;
- 
+
     // Get custom type definitions from common header
-    const auto& dataTypePoint = getPointDataType();
-    const auto& dataTypeMeasurements = getMeasurementsDataType();
-    const auto& dataTypeOpt = getOptDataType();
-    const auto& dataTypeUni = getUniDataType();
- 
+    const auto &dataTypePoint = getPointDataType();
+    const auto &dataTypeMeasurements = getMeasurementsDataType();
+    const auto &dataTypeOpt = getOptDataType();
+    const auto &dataTypeUni = getUniDataType();
+
     // Provide custom data type definitions to client
     client.setCustomDataTypes({dataTypePoint, dataTypeMeasurements, dataTypeOpt, dataTypeUni});
- 
+
     client.connect("opc.tcp://localhost:4840");
- 
+
     // Read custom variables
     opcua::Variant variant;
- 
+
     variant = client.getNode({1, "Point"}).readValue();
-    if (variant.isType(dataTypePoint)) {
-        const auto* p = static_cast<Point*>(variant.data());
+    if (variant.isType(dataTypePoint))
+    {
+        const auto *p = static_cast<Point *>(variant.data());
         std::cout << "Point:\n";
         std::cout << "- x = " << p->x << "\n";
         std::cout << "- y = " << p->y << "\n";
         std::cout << "- z = " << p->z << "\n";
     }
- 
+
     variant = client.getNode({1, "PointVec"}).readValue();
     // Variants store non-builtin data types as ExtensionObjects. If the data type is known to the
     // client/server, open62541 unwraps scalar objects transparently in the encoding layer:
@@ -47,55 +48,62 @@ int main(int argc, char* argv[]) {
     // Arrays can not be unwrapped easily, because the array is an array of ExtensionObjects.
     // The array of unwrapped objects isn't available contiguously in memory and open62541 won't
     // transparently unwrap the array. So we have to do the unwrapping ourselves:
-    if (variant.isArray() && variant.isType<opcua::ExtensionObject>()) {
+    if (variant.isArray() && variant.isType<opcua::ExtensionObject>())
+    {
         size_t i = 0;
-        for (auto&& extObj : variant.getArray<opcua::ExtensionObject>()) {
-            const auto* p = static_cast<Point*>(extObj.getDecodedData());
+        for (auto &&extObj : variant.getArray<opcua::ExtensionObject>())
+        {
+            const auto *p = static_cast<Point *>(extObj.getDecodedData());
             std::cout << "PointVec[" << i++ << "]:\n";
             std::cout << "- x = " << p->x << "\n";
             std::cout << "- y = " << p->y << "\n";
             std::cout << "- z = " << p->z << "\n";
         }
     }
- 
+
     variant = client.getNode({1, "Measurements"}).readValue();
-    if (variant.isType(dataTypeMeasurements)) {
-        const auto* m = static_cast<Measurements*>(variant.data());
+    if (variant.isType(dataTypeMeasurements))
+    {
+        const auto *m = static_cast<Measurements *>(variant.data());
         std::cout << "Measurements:\n";
         std::cout << "- description = " << m->description << "\n";
         size_t i = 0;
-        for (auto&& value : opcua::Span(m->measurements, m->measurementsSize)) {
+        for (auto &&value : opcua::Span(m->measurements, m->measurementsSize))
+        {
             std::cout << "- measurements[" << i++ << "] = " << value << "\n";
         }
     }
- 
+
     variant = client.getNode({1, "Opt"}).readValue();
-    auto formatOptional = [](const auto* ptr) {
-        return ptr == nullptr ? "NULL" : std::to_string(*ptr);
-    };
-    if (variant.isScalar() && variant.isType(dataTypeOpt)) {
-        const auto* opt = static_cast<Opt*>(variant.data());
+    auto formatOptional = [](const auto *ptr) { return ptr == nullptr ? "NULL" : std::to_string(*ptr); };
+    if (variant.isScalar() && variant.isType(dataTypeOpt))
+    {
+        const auto *opt = static_cast<Opt *>(variant.data());
         std::cout << "Opt:\n";
         std::cout << "- a = " << opt->a << "\n";
         std::cout << "- b = " << formatOptional(opt->b) << "\n";
         std::cout << "- c = " << formatOptional(opt->c) << "\n";
     }
- 
+
     variant = client.getNode({1, "Uni"}).readValue();
-    if (variant.isType(dataTypeUni)) {
-        const auto* uni = static_cast<Uni*>(variant.data());
+    if (variant.isType(dataTypeUni))
+    {
+        const auto *uni = static_cast<Uni *>(variant.data());
         std::cout << "Uni:\n";
         std::cout << "- switchField = " << static_cast<int>(uni->switchField) << "\n";
-        if (uni->switchField == UniSwitch::OptionA) {
-            std::cout << "- optionA = " << uni->fields.optionA << "\n";  // NOLINT
+        if (uni->switchField == UniSwitch::optional)
+        {
+            std::cout << "- optional = " << uni->fields.optional << "\n"; // NOLINT
         }
-        if (uni->switchField == UniSwitch::OptionB) {
-            std::cout << "- optionB = " << opcua::String(uni->fields.optionB) << "\n";  // NOLINT
+        if (uni->switchField == UniSwitch::OptionB)
+        {
+            std::cout << "- optionB = " << opcua::String(uni->fields.optionB) << "\n"; // NOLINT
         }
     }
- 
+
     variant = client.getNode({1, "Color"}).readValue();
-    if (variant.isType<int32_t>()) {
+    if (variant.isType<int32_t>())
+    {
         std::cout << "Color: " << variant.getScalar<int32_t>() << "\n";
     }
 
--- opcua_tests/src/example_server_node.cpp
+++ opcua_tests/src/example_server_node.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-#include "rclcpp/rclcpp.hpp"
-#include "open62541pp/open62541pp.h"
 #include "opcua_tests/custom_datatype.h"
+#include "open62541pp/open62541pp.h"
+#include "rclcpp/rclcpp.hpp"
 
-
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     rclcpp::init(argc, argv);
 
     rclcpp::NodeOptions options;
@@ -14,14 +14,14 @@ int main(int argc, char* argv[]) {
     auto exec = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
 
     opcua::Server server;
- 
+
     // Get custom type definitions from common header
-    const auto& dataTypePoint = getPointDataType();
-    const auto& dataTypeMeasurements = getMeasurementsDataType();
-    const auto& dataTypeOpt = getOptDataType();
-    const auto& dataTypeUni = getUniDataType();
-    const auto& dataTypeColor = getColorDataType();
- 
+    const auto &dataTypePoint = getPointDataType();
+    const auto &dataTypeMeasurements = getMeasurementsDataType();
+    const auto &dataTypeOpt = getOptDataType();
+    const auto &dataTypeUni = getUniDataType();
+    const auto &dataTypeColor = getColorDataType();
+
     // Provide custom data type definitions to server
     server.setCustomDataTypes({
         dataTypePoint,
@@ -30,7 +30,7 @@ int main(int argc, char* argv[]) {
         dataTypeUni,
         dataTypeColor,
     });
- 
+
     // Add data type nodes
     auto nodeStructureDataType = server.getNode(opcua::DataTypeId::Structure);
     nodeStructureDataType.addDataType(dataTypePoint.getTypeId(), "Point");
@@ -39,132 +39,102 @@ int main(int argc, char* argv[]) {
     nodeStructureDataType.addDataType(dataTypeUni.getTypeId(), "Uni");
     auto nodeEnumerationDataType = server.getNode(opcua::DataTypeId::Enumeration);
     nodeEnumerationDataType.addDataType(dataTypeColor.getTypeId(), "Color")
-        .addProperty(
-            {0, 0},  // auto-generate node id
-            "EnumValues",
-            opcua::VariableAttributes{}
-                .setDataType<opcua::EnumValueType>()
-                .setValueRank(opcua::ValueRank::OneDimension)
-                .setArrayDimensions({0})
-                .setValueArray(opcua::Span<const opcua::EnumValueType>{
-                    {0, {"", "Red"}, {}},
-                    {1, {"", "Green"}, {}},
-                    {2, {"", "Yellow"}, {}},
-                })
-        )
+        .addProperty({0, 0}, // auto-generate node id
+                     "EnumValues",
+                     opcua::VariableAttributes{}
+                         .setDataType<opcua::EnumValueType>()
+                         .setValueRank(opcua::ValueRank::OneDimension)
+                         .setArrayDimensions({0})
+                         .setValueArray(opcua::Span<const opcua::EnumValueType>{
+                             {0, {"", "Red"}, {}},
+                             {1, {"", "Green"}, {}},
+                             {2, {"", "Yellow"}, {}},
+                         }))
         .addModellingRule(opcua::ModellingRule::Mandatory);
- 
+
     // Add variable type nodes (optional)
     auto nodeBaseDataVariableType = server.getNode(opcua::VariableTypeId::BaseDataVariableType);
-    auto nodeVariableTypePoint = nodeBaseDataVariableType.addVariableType(
-        {1, 4243},
-        "PointType",
-        opcua::VariableTypeAttributes{}
-            .setDataType(dataTypePoint.getTypeId())
-            .setValueRank(opcua::ValueRank::ScalarOrOneDimension)
-            .setValueScalar(Point{1, 2, 3}, dataTypePoint)
-    );
-    auto nodeVariableTypeMeasurement = nodeBaseDataVariableType.addVariableType(
-        {1, 4444},
-        "MeasurementsType",
-        opcua::VariableTypeAttributes{}
-            .setDataType(dataTypeMeasurements.getTypeId())
-            .setValueRank(opcua::ValueRank::Scalar)
-            .setValueScalar(Measurements{}, dataTypeMeasurements)
-    );
-    auto nodeVariableTypeOpt = nodeBaseDataVariableType.addVariableType(
-        {1, 4645},
-        "OptType",
-        opcua::VariableTypeAttributes{}
-            .setDataType(dataTypeOpt.getTypeId())
-            .setValueRank(opcua::ValueRank::Scalar)
-            .setValueScalar(Opt{}, dataTypeOpt)
-    );
-    auto nodeVariableTypeUni = nodeBaseDataVariableType.addVariableType(
-        {1, 4846},
-        "UniType",
-        opcua::VariableTypeAttributes{}
-            .setDataType(dataTypeUni.getTypeId())
-            .setValueRank(opcua::ValueRank::Scalar)
-            .setValueScalar(Uni{}, dataTypeUni)
-    );
- 
+    auto nodeVariableTypePoint =
+        nodeBaseDataVariableType.addVariableType({1, 4243}, "PointType",
+                                                 opcua::VariableTypeAttributes{}
+                                                     .setDataType(dataTypePoint.getTypeId())
+                                                     .setValueRank(opcua::ValueRank::ScalarOrOneDimension)
+                                                     .setValueScalar(Point{1, 2, 3}, dataTypePoint));
+    auto nodeVariableTypeMeasurement =
+        nodeBaseDataVariableType.addVariableType({1, 4444}, "MeasurementsType",
+                                                 opcua::VariableTypeAttributes{}
+                                                     .setDataType(dataTypeMeasurements.getTypeId())
+                                                     .setValueRank(opcua::ValueRank::Scalar)
+                                                     .setValueScalar(Measurements{}, dataTypeMeasurements));
+    auto nodeVariableTypeOpt = nodeBaseDataVariableType.addVariableType({1, 4645}, "OptType",
+                                                                        opcua::VariableTypeAttributes{}
+                                                                            .setDataType(dataTypeOpt.getTypeId())
+                                                                            .setValueRank(opcua::ValueRank::Scalar)
+                                                                            .setValueScalar(Opt{}, dataTypeOpt));
+    auto nodeVariableTypeUni = nodeBaseDataVariableType.addVariableType({1, 4846}, "UniType",
+                                                                        opcua::VariableTypeAttributes{}
+                                                                            .setDataType(dataTypeUni.getTypeId())
+                                                                            .setValueRank(opcua::ValueRank::Scalar)
+                                                                            .setValueScalar(Uni{}, dataTypeUni));
+
     // Add variable nodes with some values
     auto nodeObjects = server.getObjectsNode();
- 
+
     const Point point{3.0, 4.0, 5.0};
-    nodeObjects.addVariable(
-        {1, "Point"},
-        "Point",
-        opcua::VariableAttributes{}
-            .setDataType(dataTypePoint.getTypeId())
-            .setValueRank(opcua::ValueRank::Scalar)
-            .setValueScalar(point, dataTypePoint),
-        nodeVariableTypePoint.getNodeId()
-    );
- 
+    nodeObjects.addVariable({1, "Point"}, "Point",
+                            opcua::VariableAttributes{}
+                                .setDataType(dataTypePoint.getTypeId())
+                                .setValueRank(opcua::ValueRank::Scalar)
+                                .setValueScalar(point, dataTypePoint),
+                            nodeVariableTypePoint.getNodeId());
+
     const std::vector<Point> pointVec{{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}};
-    nodeObjects.addVariable(
-        {1, "PointVec"},
-        "PointVec",
-        opcua::VariableAttributes{}
-            .setDataType(dataTypePoint.getTypeId())
-            .setArrayDimensions({0})  // single dimension but unknown in size
-            .setValueRank(opcua::ValueRank::OneDimension)
-            .setValueArray(pointVec, dataTypePoint),
-        nodeVariableTypePoint.getNodeId()
-    );
- 
+    nodeObjects.addVariable({1, "PointVec"}, "PointVec",
+                            opcua::VariableAttributes{}
+                                .setDataType(dataTypePoint.getTypeId())
+                                .setArrayDimensions({0}) // single dimension but unknown in size
+                                .setValueRank(opcua::ValueRank::OneDimension)
+                                .setValueArray(pointVec, dataTypePoint),
+                            nodeVariableTypePoint.getNodeId());
+
     std::vector<float> measurementsValues{19.1F, 20.2F, 19.7F};
     const Measurements measurements{
         opcua::String("Test description"),
         measurementsValues.size(),
         measurementsValues.data(),
     };
-    nodeObjects.addVariable(
-        {1, "Measurements"},
-        "Measurements",
-        opcua::VariableAttributes{}
-            .setDataType(dataTypeMeasurements.getTypeId())
-            .setValueRank(opcua::ValueRank::Scalar)
-            .setValueScalar(measurements, dataTypeMeasurements),
-        nodeVariableTypeMeasurement.getNodeId()
-    );
- 
+    nodeObjects.addVariable({1, "Measurements"}, "Measurements",
+                            opcua::VariableAttributes{}
+                                .setDataType(dataTypeMeasurements.getTypeId())
+                                .setValueRank(opcua::ValueRank::Scalar)
+                                .setValueScalar(measurements, dataTypeMeasurements),
+                            nodeVariableTypeMeasurement.getNodeId());
+
     float optC = 10.10F;
     const Opt opt{3, nullptr, &optC};
-    nodeObjects.addVariable(
-        {1, "Opt"},
-        "Opt",
-        opcua::VariableAttributes{}
-            .setDataType(dataTypeOpt.getTypeId())
-            .setValueRank(opcua::ValueRank::Scalar)
-            .setValueScalar(opt, dataTypeOpt),
-        nodeVariableTypeOpt.getNodeId()
-    );
- 
+    nodeObjects.addVariable({1, "Opt"}, "Opt",
+                            opcua::VariableAttributes{}
+                                .setDataType(dataTypeOpt.getTypeId())
+                                .setValueRank(opcua::ValueRank::Scalar)
+                                .setValueScalar(opt, dataTypeOpt),
+                            nodeVariableTypeOpt.getNodeId());
+
     Uni uni{};
     uni.switchField = UniSwitch::OptionB;
-    uni.fields.optionB = UA_STRING_STATIC("test string");  // NOLINT
-    nodeObjects.addVariable(
-        {1, "Uni"},
-        "Uni",
-        opcua::VariableAttributes{}
-            .setDataType(dataTypeUni.getTypeId())
-            .setValueRank(opcua::ValueRank::Scalar)
-            .setValueScalar(uni, dataTypeUni),
-        nodeVariableTypeUni.getNodeId()
-    );
- 
-    nodeObjects.addVariable(
-        {1, "Color"},
-        "Color",
-        opcua::VariableAttributes{}
-            .setDataType(dataTypeColor.getTypeId())
-            .setValueRank(opcua::ValueRank::Scalar)
-            .setValueScalar(Color::Green, dataTypeColor)
-    );
- 
+    uni.fields.optionB = UA_STRING_STATIC("test string"); // NOLINT
+    nodeObjects.addVariable({1, "Uni"}, "Uni",
+                            opcua::VariableAttributes{}
+                                .setDataType(dataTypeUni.getTypeId())
+                                .setValueRank(opcua::ValueRank::Scalar)
+                                .setValueScalar(uni, dataTypeUni),
+                            nodeVariableTypeUni.getNodeId());
+
+    nodeObjects.addVariable({1, "Color"}, "Color",
+                            opcua::VariableAttributes{}
+                                .setDataType(dataTypeColor.getTypeId())
+                                .setValueRank(opcua::ValueRank::Scalar)
+                                .setValueScalar(Color::Green, dataTypeColor));
+
     server.run();
 
     rclcpp::shutdown();

46869f695bc5d4de5e60df2a5b4046412050dedf
ipa-vsp <vishnu.pbhat93@gmail.com>
2024-04-29 14:03:25 +0200

    fix header file build

--- open62541pp_vendor/cmake/open62541pp_datatype_headers-extras.cmake
+++ open62541pp_vendor/cmake/open62541pp_datatype_headers-extras.cmake
@@ -2,7 +2,7 @@ macro(generate_datatype_headers TARGET)
   find_package(Python3 REQUIRED COMPONENTS Interpreter)
 
   message("Generating datatype headers for ${TARGET}...")
-  message("YAML path: ${CMAKE_CURRENT_SOURCE_DIR}/config/${TARGET}.yml")
+  message("YAML path: ${CMAKE_CURRENT_SOURCE_DIR}/config/${TARGET}.yaml")
   message("Output path: ${CMAKE_BINARY_DIR}/include/${TARGET}.hpp")
   message("Template directory: ${CMAKE_INSTALL_PREFIX}/bin/templates")
 
@@ -13,6 +13,26 @@ macro(generate_datatype_headers TARGET)
     COMMAND mkdir -p ${CMAKE_BINARY_DIR}/include
     #COMMAND mkdir -p ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/config/${TARGET}/
   )
-	message("Creating ${TARGET}_prepare target")
+
+  message("Prepare Target")
+
+  add_custom_target(
+        ${TARGET} ALL
+        DEPENDS ${TARGET}_prepare
+  )
+  
+  message("Build Header file()")
+
+  add_custom_command(
+    TARGET ${TARGET} POST_BUILD
+    COMMAND dtgen ${CMAKE_CURRENT_SOURCE_DIR}/config/${TARGET}.yaml ${CMAKE_BINARY_DIR}/include/${TARGET}.hpp
+  )
+
+  if(NOT EXISTS ${CMAKE_BINARY_DIR}/include/${TARGET}.hpp)
+    message(FATAL_ERROR "Failed to generate datatype headers from YAML")
+  endif()
+
+  install(FILES ${CMAKE_BINARY_DIR}/include/${TARGET}.hpp 
+    DESTINATION ${CMAKE_INSTALL_PREFIX}/include/${PROJECT_NAME})
   
 endmacro()
